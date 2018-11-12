b8ced941694eb6c9decc9879e8539063bd49a346
ezeiger92 <ezeiger92@gmail.com>
2018-11-11 21:27:56 -0800

    Move to 1.13

--- src/main/java/org/tyrannyofheaven/bukkit/zPermissions/region/WorldGuardRegionStrategy.java
+++ src/main/java/org/tyrannyofheaven/bukkit/zPermissions/region/WorldGuardRegionStrategy.java
@@ -32,6 +32,9 @@ import org.tyrannyofheaven.bukkit.util.ToHLoggingUtils;
 import org.tyrannyofheaven.bukkit.zPermissions.ZPermissionsCore;
 
 import com.google.common.collect.Iterables;
+import com.sk89q.worldedit.BlockVector;
+import com.sk89q.worldedit.bukkit.BukkitWorld;
+import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.managers.RegionManager;
@@ -89,9 +92,10 @@ public class WorldGuardRegionStrategy implements RegionStrategy, Listener {
     @Override
     public Set<String> getRegions(Location location, Player player) {
         if (isEnabled()) {
-            RegionManager rm = worldGuardPlugin.getRegionManager(location.getWorld());
+        	
+            RegionManager rm = WorldGuard.getInstance().getPlatform().getRegionContainer().get(new BukkitWorld(location.getWorld()));
             if (rm != null) {
-                ApplicableRegionSet ars = rm.getApplicableRegions(location);
+                ApplicableRegionSet ars = rm.getApplicableRegions(BlockVector.toBlockPoint(location.getX(), location.getY(), location.getZ()));
                 // Note, sorted from high to low priority, i.e. reverse application order
                 List<ProtectedRegion> sorted = new ArrayList<>();
                 Iterables.addAll(sorted, ars);
